629303fdb47209d19742ba8bf6285bb4b1548d4c
Muhammed Rafi C <muhammed_rafi@comcast.com>
2025-11-27 15:21:46 +0530

    RDK-59422: Provide Build to test with dns64 Proxy enabled
    Reason for change: update dual interface dns server support
    
    Signed-off-by: Muhammed Rafi C <muhammed_rafi@comcast.com>

--- recipes-connectivity/bind/files/update_namedoptions.sh
+++ recipes-connectivity/bind/files/update_namedoptions.sh
@@ -18,26 +18,31 @@
 # limitations under the License.
 ##############################################################################
 if [ -f /etc/device.properties ];then
-     . /etc/device.properties
+    . /etc/device.properties
 fi
 if [ -f /lib/rdk/t2Shared_api.sh ]; then
     source /lib/rdk/t2Shared_api.sh
 fi
+
+# Parse command line arguments
+ACTION=$1
+INTERFACE=$2
+shift 2
 DNS_SERVERS=$*
+
 LOG_FILE="/opt/logs/named.log"
-echo "`/bin/timestamp` Input parameters: $DNS_SERVERS" >> $LOG_FILE
+BUILD_CONF_PATH="/etc/bind/named.conf.options"
+SCRIPT_NAME="update_namedoptions.sh"
+
+echo "`/bin/timestamp` [$SCRIPT_NAME] Action: $ACTION, Interface: $INTERFACE, DNS Servers: $DNS_SERVERS" >> $LOG_FILE
+
+if [ "x$ACTION" != "xadd" ] && [ "x$ACTION" != "xremove" ]; then
+    echo "`/bin/timestamp` [$SCRIPT_NAME] ERROR: Invalid action '$ACTION'. Must be 'add' or 'remove'" >> $LOG_FILE
+    exit 1
+fi
+
 DNS64_SERVER1=`tr181 -g Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.DNS64Proxy.Server1 2>&1`
-if [ "x$DNS64_SERVER1" = "x" ]; then
-    DNS64_SERVER1="2a00:1098:2b::1" # nat64.net	Amsterdam
-    echo "`/bin/timestamp` DNS64 Server1 not configured, using default: $DNS64_SERVER1" >> $LOG_FILE
-fi
-
 DNS64_SERVER2=`tr181 -g Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.DNS64Proxy.Server2 2>&1`
-if [ "x$DNS64_SERVER2" = "x" ]; then
-    DNS64_SERVER2="2a01:4ff:f0:9876::1" # nat64.net	Ashburn
-    echo "`/bin/timestamp` DNS64 Server2 not configured, using default: $DNS64_SERVER2" >> $LOG_FILE
-fi
-
 DNS64_SERVER3=`tr181 -g Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.DNS64Proxy.Server3 2>&1`
 DNS64_SERVER4=`tr181 -g Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.DNS64Proxy.Server4 2>&1`
 
@@ -49,52 +54,85 @@ APPENDER_STR="{ \n clients { any; };\n exclude { 64:FF9B::/96; ::ffff:0000:0000/
 DNS64_STR=""
 if [ "x$DNS64_SERVER1" != "x" ]; then
     DNS64_STR="$DNS64_STR \ndns64 $DNS64_SERVER1 $APPENDER_STR"
-    echo "`/bin/timestamp` DNS64 Proxy Server1: $DNS64_SERVER1" >> $LOG_FILE
+    echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Proxy Server1: $DNS64_SERVER1" >> $LOG_FILE
 fi
 if [ "x$DNS64_SERVER2" != "x" ]; then
     DNS64_STR="$DNS64_STR \ndns64 $DNS64_SERVER2 $APPENDER_STR"
-    echo "`/bin/timestamp` DNS64 Proxy Server2: $DNS64_SERVER2" >> $LOG_FILE
+    echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Proxy Server2: $DNS64_SERVER2" >> $LOG_FILE
 fi
 if [ "x$DNS64_SERVER3" != "x" ]; then
     DNS64_STR="$DNS64_STR \ndns64 $DNS64_SERVER3 $APPENDER_STR"
-    echo "`/bin/timestamp` DNS64 Proxy Server3: $DNS64_SERVER3" >> $LOG_FILE
+    echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Proxy Server3: $DNS64_SERVER3" >> $LOG_FILE
 fi
 if [ "x$DNS64_SERVER4" != "x" ]; then
     DNS64_STR="$DNS64_STR \ndns64 $DNS64_SERVER4 $APPENDER_STR"
-    echo "`/bin/timestamp` DNS64 Proxy Server4: $DNS64_SERVER4" >> $LOG_FILE
+    echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Proxy Server4: $DNS64_SERVER4" >> $LOG_FILE
 fi
 
 if [ "x$DNS64_STR" = "x" ]; then
-    echo "`/bin/timestamp` No DNS64 Proxy servers configured" >> $LOG_FILE
+    echo "`/bin/timestamp` [$SCRIPT_NAME] No DNS64 Proxy servers configured" >> $LOG_FILE
 fi
 
 if [ "x$RFC_BIND_ENABLED" = "xtrue" ]; then
-	BUILD_CONF_PATH="/tmp/named.conf.options"
-	# Refresh the options again. Since resolver config can change while box is running.
-	# Added the following to handle that case.
-	/bin/umount /etc/bind/named.conf.options
-	rm -f $BUILD_CONF_PATH
-	/sbin/mount-copybind  $BUILD_CONF_PATH /etc/bind/named.conf.options
-	sed -i "s#\/\/OPT#$DNS_SERVERS#g" $BUILD_CONF_PATH
-	if [ "x$DNS64_STR" != "x" ];  then
-		sed -i "s#\/\/DNS64#$DNS64_STR#g" $BUILD_CONF_PATH
-		echo "DNS64 Servers are Configured" >>$LOG_FILE
-	else
-		echo "DNS64 Servers not Configured" >>$LOG_FILE
+
+    # Refresh the options again. Since resolver config can change while box is running.
+    # Added the following to handle that case.
+
+    if [ "$ACTION" = "remove" ]; then
+        # Remove DNS servers for the specified interface
+        if [ "$INTERFACE" = "eth0" ]; then
+            sed -i '/\/\/eth0/,/\/\/wlan0/c\ \t\t//eth0\n\t\t//wlan0' $BUILD_CONF_PATH
+            echo "`/bin/timestamp` [$SCRIPT_NAME] Removed DNS servers for eth0" >> $LOG_FILE
+        else
+            sed -i '/\/\/wlan0/,/\};/c\ \t\t//wlan0\n\t};' $BUILD_CONF_PATH
+            echo "`/bin/timestamp` [$SCRIPT_NAME] Removed DNS servers for wlan0" >> $LOG_FILE
+        fi
+    elif [ "$ACTION" = "add" ]; then
+        # Add DNS servers for the specified interface
+        if [ "x$DNS_SERVERS" = "x" ]; then
+            echo "`/bin/timestamp` [$SCRIPT_NAME] ERROR: No DNS servers provided for add action" >> $LOG_FILE
+            exit 1
+        fi
+
+        # Add DNS servers one by one if not present
+        for DNS in $DNS_SERVERS; do
+            # Skip empty entries
+            if [ "x$DNS" = "x" ]; then
+                continue
+            fi
+
+            if grep -q "$DNS" $BUILD_CONF_PATH; then
+                echo "`/bin/timestamp` [$SCRIPT_NAME] DNS server $DNS already present, skipping" >> $LOG_FILE
+            else
+                if [ "$INTERFACE" = "eth0" ]; then
+                    sed -i "/\/\/eth0/a\ \t\t$DNS;" $BUILD_CONF_PATH
+                else
+                    sed -i "/\/\/wlan0/a\ \t\t$DNS;" $BUILD_CONF_PATH
+                fi
+                echo "`/bin/timestamp` [$SCRIPT_NAME] Added DNS server $DNS for $INTERFACE" >> $LOG_FILE
+            fi
+        done
     fi
-    cat /tmp/named.conf.options >/etc/bind/named.conf.options
+
+    if [ "x$DNS64_STR" != "x" ];  then
+        sed -i "s#\/\/DNS64#$DNS64_STR#g" $BUILD_CONF_PATH
+        echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Servers are Configured" >>$LOG_FILE
+    else
+        echo "`/bin/timestamp` [$SCRIPT_NAME] DNS64 Servers not Configured, Empty" >>$LOG_FILE
+    fi
+
     if [ -f /usr/sbin/named -o -f /media/apps/bind-dl/usr/sbin/named ];then
         pkill -HUP named
         systemctl restart named.service
-        echo "`/bin/timestamp` Bind Support is enabled, named is used for  DNS Resolutions." >> $LOG_FILE
+        echo "`/bin/timestamp` [$SCRIPT_NAME] Bind Support is enabled, named is used for  DNS Resolutions." >> $LOG_FILE
         if [ -f /lib/rdk/logMilestone.sh ]; then
             sh /lib/rdk/logMilestone.sh "NAMED_STARTED"
         fi
     else
-        echo "`/bin/timestamp` Bind Support is enabled, named binary is not present, dns will fail" >> $LOG_FILE
+        echo "`/bin/timestamp` [$SCRIPT_NAME] Bind Support is enabled, named binary is not present, dns will fail" >> $LOG_FILE
     fi
 else
-	# This is to make sure atleast dnsmasq runs even if it fails initially. or some switch happened inbetween.
-	echo "`/bin/timestamp` Bind Support is not enabled" >> $LOG_FILE
-	systemctl stop named.service
+    # This is to make sure atleast dnsmasq runs even if it fails initially. or some switch happened inbetween.
+    echo "`/bin/timestamp` [$SCRIPT_NAME] Bind Support is not enabled" >> $LOG_FILE
+    systemctl stop named.service
 fi
--- recipes-connectivity/networkmanager/networkmanager/99-update-named.sh
+++ recipes-connectivity/networkmanager/networkmanager/99-update-named.sh
@@ -24,39 +24,63 @@
 # Note: NetworkManager updates /var/run/NetworkManager/no-stub-resolv.conf
 # which is symlinked to /etc/resolv.dnsmasq
 
+SCRIPT_NAME="[nm-dispatcher]"
 INTERFACE=$1
 ACTION=$2
 LOG_FILE="/opt/logs/named.log"
 
-echo "`/bin/timestamp` nm-dispatcher: Interface=$INTERFACE Action=$ACTION" >> $LOG_FILE
+echo "`/bin/timestamp` $SCRIPT_NAME: Interface=$INTERFACE Action=$ACTION" >> $LOG_FILE
+
+# Determine interface type (eth0 or wlan0)
+IFACE_TYPE=""
+case "$INTERFACE" in
+    eth*)
+        IFACE_TYPE="eth0"
+        ;;
+    wlan*)
+        IFACE_TYPE="wlan0"
+        ;;
+    *)
+        echo "`/bin/timestamp` $SCRIPT_NAME: named interface type: $INTERFACE" >> $LOG_FILE
+        exit 0
+        ;;
+esac
 
 case "$ACTION" in
     up|dhcp4-change|dhcp6-change)
         # Check if resolv.dnsmasq exists and has DNS servers
         if [ -f /etc/resolv.dnsmasq ] && [ -s /etc/resolv.dnsmasq ]; then
-            # Extract DNS servers with semicolons
             DNS_ADDR=""
             while read -r line; do
                 ADDR=$(echo $line | cut -d " " -f2)
                 if [ "$line" != "${line#*[0-9].[0-9]}" ]; then
                     # IPv4
-                    DNS_ADDR="$DNS_ADDR $ADDR;"
-                    echo "`/bin/timestamp` nm-dispatcher: Found IPv4 DNS server: $ADDR" >> $LOG_FILE
+                    DNS_ADDR="$DNS_ADDR $ADDR"
+                    echo "`/bin/timestamp` $SCRIPT_NAME: Found IPv4 DNS server: $ADDR" >> $LOG_FILE
                 elif [ "$line" != "${line#*:[0-9a-fA-F]}" ]; then
                     # IPv6
-                    DNS_ADDR="$DNS_ADDR $ADDR;"
-                    echo "`/bin/timestamp` nm-dispatcher: Found IPv6 DNS server: $ADDR" >> $LOG_FILE
+                    DNS_ADDR="$DNS_ADDR $ADDR"
+                    echo "`/bin/timestamp` $SCRIPT_NAME: Found IPv6 DNS server: $ADDR" >> $LOG_FILE
                 fi
             done < /etc/resolv.dnsmasq
 
-            # Call update_namedoptions.sh with DNS servers
-            if [ -n "$DNS_ADDR" ]; then
+            # Call update_namedoptions.sh with interface-specific DNS servers
+            if [ -n "$DNS_ADDR" ] && [ -n "$IFACE_TYPE" ]; then
                 if [ -f /lib/rdk/update_namedoptions.sh ]; then
-                    echo "`/bin/timestamp` nm-dispatcher: Updating BIND configuration with DNS servers: $DNS_ADDR" >> $LOG_FILE
-                    /bin/sh /lib/rdk/update_namedoptions.sh $DNS_ADDR
-                else
-                    echo "`/bin/timestamp` nm-dispatcher: ERROR - /lib/rdk/update_namedoptions.sh not found" >> $LOG_FILE
+                    echo "`/bin/timestamp` $SCRIPT_NAME: Adding DNS servers for $IFACE_TYPE: $DNS_ADDR" >> $LOG_FILE
+                    /bin/sh /lib/rdk/update_namedoptions.sh add $IFACE_TYPE $DNS_ADDR
                 fi
+            else
+                echo "`/bin/timestamp` $SCRIPT_NAME: No DNS servers found " >> $LOG_FILE
+            fi
+        fi
+        ;;
+    down)
+        # When interface goes down, remove its DNS servers
+        if [ -n "$IFACE_TYPE" ]; then
+            if [ -f /lib/rdk/update_namedoptions.sh ]; then
+                echo "`/bin/timestamp` $SCRIPT_NAME: Removing DNS servers for $IFACE_TYPE (interface down)" >> $LOG_FILE
+                /bin/sh /lib/rdk/update_namedoptions.sh remove $IFACE_TYPE
             fi
         fi
         ;;
