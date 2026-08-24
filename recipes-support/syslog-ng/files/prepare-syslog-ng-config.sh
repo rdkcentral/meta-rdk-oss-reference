a9381ccb0a6204d73110a36b36b1675e1867dca0
Konkati_Bhavani <Konkati_Bhavani@comcast.com>
2026-08-24 12:41:55 +0000

    Change for RFC for Boot_id

--- recipes-support/syslog-ng/files/prepare-syslog-ng-config.sh
+++ recipes-support/syslog-ng/files/prepare-syslog-ng-config.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+DISABLE_LOGCHRONO="/opt/secure/RFC/disable_logchrono"
+SYSLOG_NG_CONF="/tmp/syslog-ng.conf"
+
 mkdir -p /tmp/syslog-ng
 if [ ! -f "${SYSLOG_NG_CONF}" ]; then
    cp /etc/syslog-ng/syslog-ng.conf "${SYSLOG_NG_CONF}"
 fi
 
-DISABLE_LOGCHRONO="/opt/secure/RFC/disable_logchrono"
-SYSLOG_NG_CONF="/tmp/syslog-ng.conf"
 
 GENERAL_TEMPLATE='template-function t_rdk "${S_YEAR}-${S_MONTH}-${S_DAY}T${S_HOUR}:${S_MIN}:${S_SEC}.${S_MSEC}Z ${MSGHDR} ${MSG}";'
 
